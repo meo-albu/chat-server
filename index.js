7557b6681af8da66b6bf88ba37115653f7ba7d11
Marian Albu <albumeo1990@gmail.com>
2020-11-29 20:52:40 +0200

    added socket io

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require('http');
+const server = http.createServer(app)
+const port = 8888
+const socketio = require('socket.io')
+
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+})
+
+io.on('connection', socket => {
+  console.log(socket.id)
+
+  socket.emit('message', 'welcome ' + socket.id)
+  socket.broadcast.emit('message', socket.id + ' has connected')
+
+  socket.on('disconnect', socket => {
+    io.emit('message', socket.id + ' has disconnected')
+    console.log(socket.id + ' disconnected...');
+  });
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
